2f8dac29941b2ba9ae3c16b2e272ab8d17f60299
Bhanshuu <bhanshuchaudhary0808@gmail.com>
2025-02-07 09:43:33 +0530

    day2

--- day2/p2.js
+++ day2/p2.js
@@ -1,2 +1,8 @@
-const a = "hello";
-console.log(a);
+const fs=require('fs');
+
+const newdata="I am new data";
+// const data=fs.readFileSync('./data.txt', 'utf8');
+
+fs.writeFileSync('./data.txt', newdata);
+
+// console.log(data);
